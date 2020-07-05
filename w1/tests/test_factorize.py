545afbcb2db224e7b48ecf8dddac5a43a3b0b3f4
gorskii <egor.yakayaka@gmail.com>
2020-07-05 20:58:32 +0300

    Declare sample test cases

--- w1/tests/test_factorize.py
+++ w1/tests/test_factorize.py
@@ -2,7 +2,23 @@ import unittest
 
 
 class TestFactorize(unittest.TestCase):
-    pass
+    def test_wrong_types_raise_exception(self):
+        self.fail()
+
+    def test_negative(self):
+        self.fail()
+
+    def test_zero_and_one_cases(self):
+        self.fail()
+
+    def test_simple_numbers(self):
+        self.fail()
+
+    def test_two_simple_multipliers(self):
+        self.fail()
+
+    def test_many_multipliers(self):
+        self.fail()
 
 
 if __name__ == '__main__':
