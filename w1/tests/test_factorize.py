337c6bf6d7f298e86819e4969c2003adc08db8ae
gorskii <egor.yakayaka@gmail.com>
2020-07-06 03:03:37 +0300

    Rename 'case' variable to 'x' to meet task restrictions

--- w1/tests/test_factorize.py
+++ w1/tests/test_factorize.py
@@ -6,43 +6,43 @@ from w1.factorize import factorize
 class TestFactorize(unittest.TestCase):
     def test_wrong_types_raise_exception(self):
         self.cases = ('string', 1.5)
-        for case in self.cases:
-            with self.subTest(case=case):
-                self.assertRaises(TypeError, factorize, case)
+        for x in self.cases:
+            with self.subTest(x=x):
+                self.assertRaises(TypeError, factorize, x)
 
     def test_negative(self):
         self.cases = (-1, -10, -100)
-        for case in self.cases:
-            with self.subTest(case=case):
-                self.assertRaises(ValueError, factorize, case)
+        for x in self.cases:
+            with self.subTest(x=x):
+                self.assertRaises(ValueError, factorize, x)
 
     def test_zero_and_one_cases(self):
         self.cases = (0, 1)
         self.expected = ((0,), (1,))
-        for case, result in zip(self.cases, self.expected):
-            with self.subTest(case=case):
-                self.assertEqual(factorize(case), result)
+        for x, result in zip(self.cases, self.expected):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), result)
 
     def test_simple_numbers(self):
         self.cases = (3, 13, 29)
         self.expected = ((3,), (13,), (29,))
-        for case, result in zip(self.cases, self.expected):
-            with self.subTest(case=case):
-                self.assertEqual(factorize(case), result)
+        for x, result in zip(self.cases, self.expected):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), result)
 
     def test_two_simple_multipliers(self):
         self.cases = (6, 26, 121)
         self.expected = ((2, 3), (2, 13), (11, 11))
-        for case, result in zip(self.cases, self.expected):
-            with self.subTest(case=case):
-                self.assertEqual(factorize(case), result)
+        for x, result in zip(self.cases, self.expected):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), result)
 
     def test_many_multipliers(self):
         self.cases = (1001, 9699690)
         self.expected = ((7, 11, 13), (2, 3, 5, 7, 11, 13, 17, 19))
-        for case, result in zip(self.cases, self.expected):
-            with self.subTest(case=case):
-                self.assertEqual(factorize(case), result)
+        for x, result in zip(self.cases, self.expected):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), result)
 
 
 if __name__ == '__main__':
