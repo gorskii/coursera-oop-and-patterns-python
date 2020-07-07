ecef176c9ea47e465535d86f5672604ac8ff44f2
gorskii <egor.yakayaka@gmail.com>
2020-07-07 21:03:07 +0300

    Import abc and implement abstract base class Base with abstract methods.
    Classes A, B and C implement Base.
    Some of them use default implementation of Base methods,
    implementing a framework pattern.

--- w2/w2_abc.py
+++ w2/w2_abc.py
@@ -1,35 +1,41 @@
 import math
+from abc import ABC, abstractmethod
 
 
-class Base:
-    pass
-
-
-class A:
+class Base(ABC):
     def __init__(self, data, result):
         self.data = data
         self.result = result
 
+    @abstractmethod
     def get_answer(self):
         return [int(x >= 0.5) for x in self.data]
 
+    @abstractmethod
+    def get_loss(self):
+        return sum([(x - y) * (x - y) for (x, y) in zip(self.data, self.result)])
+
+    @abstractmethod
     def get_score(self):
         ans = self.get_answer()
         return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
                / len(ans)
 
-    def get_loss(self):
-        return sum(
-            [(x - y) * (x - y) for (x, y) in zip(self.data, self.result)])
 
-
-class B:
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
+class A(Base):
     def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
+        return super().get_answer()
+
+    def get_score(self):
+        return super().get_score()
+
+    def get_loss(self):
+        return super().get_loss()
+
+
+class B(Base):
+    def get_answer(self):
+        return super().get_answer()
 
     def get_loss(self):
         return -sum([
@@ -53,18 +59,12 @@ class B:
         return 2 * pre * rec / (pre + rec)
 
 
-class C:
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
+class C(Base):
     def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
+        return super().get_answer()
 
     def get_score(self):
-        ans = self.get_answer()
-        return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
-               / len(ans)
+        return super().get_score()
 
     def get_loss(self):
         return sum([abs(x - y) for (x, y) in zip(self.data, self.result)])
